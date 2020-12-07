f5542a81196c7bb4b07874633e1b9752aeeb426a
Your Name <you@example.com>
2020-12-06 18:02:01 -0600

    Made DataFrame.

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = '2561a1e138fabe170c5f9f05a8838345'
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCm2X41KvRKW-61Hxp-R8JvsFLJAobQUKs"
